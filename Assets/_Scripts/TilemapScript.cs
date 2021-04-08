3247c466b1d23c0ea5646378276d79ad5175230e
Henry Rocha <henryfr1@al.insper.edu.br>
2021-04-07 22:44:50 -0300

    Add basic tower placement

---
+++ Assets/_Scripts/TilemapScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapScript : MonoBehaviour
+{
+    [SerializeField]  private GameObject towerToPlace;
+    private Tilemap tilemap;
+    private Grid grid;
+    private bool[,] towerPlacements;
+
+    // Start is called  before the first frame update
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+        grid = transform.parent.GetComponentInParent<Grid>();
+
+
+        Debug.Log($"{tilemap.cellBounds}");
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Click on tilemap!");
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int cellGridPos = grid.WorldToCell(mousePos);
+        Vector3 cellWorldPos = grid.CellToWorld(cellGridPos);
+        TileBase clickedTile = tilemap.GetTile(cellGridPos);
+
+        cellWorldPos += grid.cellSize / 2;
+        // Debug.Log(cellWorldPos.y);
+        cellWorldPos.y += grid.cellSize.y / 2;
+        Debug.Log(cellWorldPos);
+
+        if (clickedTile.name == "52")
+        {
+            Instantiate(towerToPlace, cellWorldPos, Quaternion.identity);
+        }
+    }
+}
